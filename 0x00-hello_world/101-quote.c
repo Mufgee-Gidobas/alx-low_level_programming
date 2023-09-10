148000966b6a5162dd291a4ef8a584f66f573df0
Mufgee-Gidobas <mfaroukugwandu02mg@yahoo.com>
2023-09-10 06:03:32 -0700

    UNIX is a simple OS, only the genius understand its simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
- * main- a c programm using unix to prine a lineto std error without put, printf funcions
+ * main- a c programm using unix to std error
  * Return: 1 (success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19", 59);
+write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
