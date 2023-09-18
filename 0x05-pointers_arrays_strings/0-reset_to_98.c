c82a6cae2f71405048b22cc16393bcf95056745a
Mufgee-Gidobas <mfaroukugwandu02mg@yahoo.com>
2023-09-18 08:30:56 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include<main.h>
+/**
+ * reset to 98 - function that takes int to a parameter
+ * value is updated to point to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
